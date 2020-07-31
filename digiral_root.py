e287270161d7fa9edc0b2e57bc368ca74ad99079
GeskO <Maxirosa92@gmail.com>
2020-07-31 17:06:54 +0300

    Добавлена задача Digital Root

---
+++ digiral_root.py
@@ -0,0 +1,51 @@
+def digital_root(n):
+    first = n
+    res = []
+    while n > 10:
+        arr = []
+        while n > 0:
+            arr.insert(0, n % 10)
+            n = n // 10
+        sum = 0
+        for i in arr:
+            sum += i
+        n = sum
+        res.append(arr)
+    print(f"{first}", end=" ")
+    for row in res:
+        print("-->", end=" ")
+        print(' + '.join([str(elem) for elem in row]), end=" ")
+    print(f"= {sum}")
+    return sum
+    # print(f"{res}")
+
+# a = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+# s = 0
+# for row in a:
+#     for elem in row:
+#         s += elem
+# print(s)
+
+# 45
+
+# a = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+# for i in range(len(a)):
+#     for j in range(len(a[i])):
+#         print(a[i][j], end=' ')
+#     print()
+
+# 1 2 3 4
+# 5 6
+# 7 8 9
+
+# 16  -->  1 + 6 = 7
+# 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+# 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+# 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+
+
+digital_root(16)
+digital_root(942)
+digital_root(132189)
+digital_root(493193)
